54b7a49158f1a52fa1fd60ef5d8da6ea0ab81f07
Booster2ooo <the._.truth@hotmail.fr>
2019-06-25 23:08:13 +0200

    Using "extra flash" for config storage
    The STM32F103C8T6 is advertised as a chip with 64k of flash memory but it have 128k. flash_storage.h exploits this extra memory to save the config on a segment that will not be rewritten when updating the program. When the chip will be flashed with a new "64k" program, the config will be safe on the extra memory and won't be overwritten.

--- Inc/flash_storage.h
+++ Inc/flash_storage.h
@@ -2,7 +2,7 @@
 #include <string.h>
 #include "stm32f1xx_hal.h"
 
-#define FLASH_END_ADDRESS			((uint32_t)0x08010000)
+#define FLASH_END_ADDRESS			((uint32_t)0x08020000)
 #define FLASH_STORAGE_START_ADDRESS		FLASH_END_ADDRESS - (FLASH_PAGE_SIZE)
 
 void Write_Flash_Config(uint32_t address, void *config_ptr, uint8_t size);
